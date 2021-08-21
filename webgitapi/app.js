4a0133ab2ccc5537e9278c0ca0705592c4f784c0
Daniel Viscarra <66076219+danielon20@users.noreply.github.com>
2021-08-21 18:44:44 -0500

    Implementacion de tokens y validacion por token y rol

--- webgitapi/app.js
+++ webgitapi/app.js
@@ -7,6 +7,7 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usuariosRouter = require('./routes/usuarios');
+var authRouter = require('./routes/auth')
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/usuarios', usuariosRouter)
+app.use('/auth', authRouter)
 
 module.exports = app;
--- webgitapi/routes/usuarios.js
+++ webgitapi/routes/usuarios.js
@@ -1,47 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const sequelize = require('../models/index.js').sequelize;
-var initModels = require("../models/init-models");
-var models = initModels(sequelize); 
+var usuariosController = require('../controllers/usuariosController');
 
+const auth = require("../middleware/auth");
+const checkRole = require("../middleware/role")
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  models.usuarios.findAll({ 
-    attributes: { exclude: ["updatedAt"] }
-  })
-  .then(usuarios => {
-     res.send(usuarios)
-  })
-  .catch(error => res.status(400).send(error))
-});
+//Para realizar esta accion necesitas tener un token y que tu role sea uno de los que esta en la lista: ['Administrador','Usuario']
+router.get('/', [auth,checkRole(['Administrador','Usuario'])],usuariosController.list);
+/* GET by ID home page. */
+//Para realizar esta accion solo necesitas tener un token
+router.get('/:id', auth ,usuariosController.show);
+/* POST */
+router.post('/',  usuariosController.create);
+
+
+
 
 /*
-router.get('/cliente/:clienteId', function(req, res, next) {
-  models.ordenes.findAll({ 
-    where: {
-      clienteId: req.params.clienteId
-    },
-    attributes: { exclude: ["updatedAt"] }   
-  })
-  .then(ordenes => {
-     res.send(ordenes)
-  })
-  .catch(error => res.status(400).send(error))
-});
-
-router.post('/', function(req, res, next) {
-  models.ordenes.create({
-    fecha: new Date(), clienteId: 9, createdAt: new Date(), updatedAt: new Date()
-  })  
-  //res.json({message : 'Orden guardada'})
-  .then(()=>{
-    res.json({message : 'Orden guardada'})
-    //console.log('Orden saved')
-    //res.send('ddd')
-  })
-  .catch(error => res.status(400).send(error))
-});
-
 router.put('/', function(req, res, next) {
   models.ordenes.update({ fecha: new Date() }, {
     where: {
