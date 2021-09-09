b4c117b0c592c89a3de9b6dd819bb506f1fc3f37
Tommy Beltrán <tomdbelt@espol.edu.ec>
2021-09-08 22:12:13 -0500

    Implementado servicio de emails

--- webgitapi/app.js
+++ webgitapi/app.js
@@ -3,6 +3,23 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+//const nodemailer = require('nodemailer');
+
+//var mail = require('./config/mailer')();
+//mail.send();
+
+
+/*
+const mailTransporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'team.webgit@gmail.com',
+        pass: 'mbblggifsakpgvwj'
+    }
+});
+*/
 
 var indexRouter = require('./routes/index');
 var registroCursoRouter = require('./routes/registro_cursos');
@@ -13,9 +30,11 @@ var reporteventasRoutes = require('./routes/reporteventasRoutes');
 var cursosRoutes = require('./routes/cursos');
 var tareasRoutes = require('./routes/tarea');
 var contactosRoutes = require('./routes/contactos');
+var emailRoutes = require('./routes/email');
 
 var app = express();
 const nodb = require("./models");
+const mailTransporter = require('./config/mailer');
 
 app.use(cors());
 app.use(logger('dev'));
@@ -33,5 +52,6 @@ app.use('/registro_curso', registroCursoRouter);
 app.use('/cursos', cursosRoutes);
 app.use('/tareas', tareasRoutes);
 app.use('/contactos', contactosRoutes);
+app.use('/email', emailRoutes);
 
 module.exports = app;
